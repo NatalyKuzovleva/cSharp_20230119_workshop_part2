d3839a285b63f6b02108b5e8e2b7bae70111f3c5
Nataly <nt.kuzovleva@gmail.com>
2023-01-28 01:52:17 +0300

    В Task42 добавлен 2й вариант решения

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -17,23 +17,42 @@ int EnterNumberRequest(string text)
 
 
 // Метод преобразовывает десятичное число в двоичное
-string ConvertDecToBin(int num)
+// Вариант 1. Через цикл while
+// string ConvertDecToBin(int num)
+// {
+//     string result = string.Empty;   // создаём переменную для строки и объявляем её пустой, 
+//                                     // как в случае с переменной для числа - кладём туда ноль
+//     while (num > 0)
+//     {
+//         result = num % 2 + result;
+//         num /= 2;       // num = num/2;
+//     }
+//     return result;
+// }
+
+
+// Метод преобразовывает десятичное число в двоичное
+// Вариант 2. Через рекурсию
+
+int ConvertDecToBin(int num)
 {
-    string result = string.Empty;   // создаём переменную для строки и объявляем её пустой, 
-                                    // как в случае с переменной для числа - кладём туда ноль
-    while (num > 0)
-    {
-        result = num % 2 + result;
-        num /= 2;       // num = num/2;
-    }
+    if (num ==0) return 0;
+    int result = num % 2;
+    num /= 2;
+    result += ConvertDecToBin(num) *10;
     return result;
 }
 
 /***************************************************************/
 
 int number = EnterNumberRequest("Введите десятичное число: ");
-string bin = ConvertDecToBin(number);
+//string bin = ConvertDecToBin(number);
+
+int bin = ConvertDecToBin(number);
 Console.WriteLine($"{number} -> {bin}");
 
 // Console.WriteLine(ConvertDecToBin(2));
 // Console.WriteLine(ConvertDecToBin(number));
+
+// Console.WriteLine(ConvertDecToBin(Convert.ToInt32(2)));
+// Console.WriteLine(ConvertDecToBin(Convert.ToInt32(number)));
