ac9c3d4afccefb1a5adb60cb1a20e2ef846fe977
Nataly <nt.kuzovleva@gmail.com>
2023-01-31 01:13:03 +0300

    Решена задача в Task49

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -2,7 +2,66 @@
 // которых оба индекса чётные, и замените эти элементы на их
 // квадраты.
 // Например, изначально массив
-// выглядел вот так:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
+// выглядел вот так:            новый массив выглядит так:
+// 1 4 7 2                        1  4 49  2
+// 5 9 2 3                        5  9  2  3
+// 8 4 2 4                       64  4  4  4
+
+
+/************************* М Е Т О Д Ы *************************/
+
+// Создание двумерного массива, заполненного псевдослучайными целыми числами
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];     // создаём двумерный массив
+    Random rnd = new Random();                  // создаём экземпляр класса Рандом
+
+    for (int i = 0; i < matrix.GetLength(0); i++)       // matrix.GetLength(0) - нулевое измерение это строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)   // matrix.GetLength(1) - первое измерение это столбцы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+// Вывод двумерного числового массива на печать в консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            //if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},"); // в интерполяцию добавили форматирование
+            // без разделительной запятой между элементами if-else не нужен! 
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+// Метод заменяет элементы, у которых оба индекса чётные, на их квадраты
+void MatrixSquareEvenElements(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+/***************************************************************/
+Console.Clear();
+int[,] array2D = CreateMatrixRndInt(3, 4, -15, 15);
+PrintMatrix(array2D);
+Console.WriteLine();
+
+MatrixSquareEvenElements(array2D);
+PrintMatrix(array2D);
