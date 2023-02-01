83fc94bf9818201d61f45a816da904df3644fe7f
Nataly <nt.kuzovleva@gmail.com>
2023-02-02 00:30:10 +0300

    Решена задача в Task53

--- Task53/Program.cs
+++ Task53/Program.cs
@@ -2,3 +2,62 @@
 // которая поменяет местами первую и последнюю строку
 // массива.
 
+
+/************************* М Е Т О Д Ы *************************/
+
+// Создание двумерного массива, заполненного псевдослучайными целыми числами
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];     // создаём двумерный массив
+    Random rnd = new Random();                  // создаём экземпляр класса Рандом
+
+    for (int i = 0; i < matrix.GetLength(0); i++)       // matrix.GetLength(0) - нулевое измерение это строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)   // matrix.GetLength(1) - первое измерение это столбцы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+// Вывод двумерного числового массива на печать в консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            //if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},"); // в интерполяцию добавили форматирование
+            // без разделительной запятой между элементами if-else не нужен! 
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+// Метод меняет местами первую и последнюю строку массива
+void ReplaceFirstAndLastRows(int[,] matrix)
+{
+    int temp = 0;
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+
+/***************************************************************/
+Console.Clear();
+int[,] array2D = CreateMatrixRndInt(3, 4, -15, 15);
+PrintMatrix(array2D);
+Console.WriteLine();
+ReplaceFirstAndLastRows(array2D);
+PrintMatrix(array2D);
