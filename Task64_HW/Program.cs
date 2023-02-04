597c69ac186f0380a38802c854cded4a6e6bde95
Nataly <nt.kuzovleva@gmail.com>
2023-02-05 02:07:21 +0300

    Решена задача в Task64

--- Task64_HW/Program.cs
+++ Task64_HW/Program.cs
@@ -4,3 +4,35 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+/************************* М Е Т О Д Ы *************************/
+
+// Метод выводит в консоль текст на запрос ввода числа от пользователя
+int EnterNumberRequest(string text)
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+// Метод выводит все натуральные числа в промежутке от N до 1
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    if (num > 1) Console.Write($"{num}, ");
+    else Console.Write($"{num}");
+    NaturalNumber(num - 1);
+}
+
+/***************************************************************/
+Console.Clear();
+int number = EnterNumberRequest("Введите натуральное число: ");
+if (number <= 0) Console.WriteLine("Введено не натуральное число. Проверьте данные!");
+else
+{
+    Console.Write($"N = {number} -> \"");
+    NaturalNumber(number);
+    Console.Write("\"");
+}
